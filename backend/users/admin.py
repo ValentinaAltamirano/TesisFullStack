4a2669b827d3510745d3912beadc433242a515d6
Metre <lucas.metrebian@gmail.com>
2024-02-05 16:21:55 -0300

    Implementar ADM

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,2 +1,24 @@
 from django.contrib import admin
-from .models import Empresario
+from .models import *
+
+# Register your models here.
+admin.site.register(Empresario)
+admin.site.register(Establecimiento)
+admin.site.register(MetodoDePago)
+admin.site.register(RedSocial)
+
+
+#imagenes y Relaciones:
+admin.site.register(Imagen)
+admin.site.register(ImagenXEstablecimiento)
+admin.site.register(EstablecimientoXMetodoPago)
+admin.site.register(EstablecimientoXRedSocial)
+
+#tipos y Servicios:
+admin.site.register(TipoEstablecimiento)
+admin.site.register(TipoAlojamiento)
+admin.site.register(TipoServicio)
+
+#Alojamientos y servicios:
+admin.site.register(Alojamiento)
+admin.site.register(AlojamientoXTipoServicio)
