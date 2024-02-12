ba8dc1c5c9167dbea71b9987067c97ad42444618
ValentinaAltamirano <107081335+ValentinaAltamirano@users.noreply.github.com>
2024-02-12 18:17:50 -0300

    Implementar interfaz de detalle de alojamiento

--- frontend/src/app/detalle-alojamiento/detalle-alojamiento.component.ts
+++ frontend/src/app/detalle-alojamiento/detalle-alojamiento.component.ts
@@ -11,42 +11,58 @@ import { AlojamientoService } from '../service/alojamiento.service';
 export class DetalleAlojamientoComponent {
 
   establecimientoId: number;
-  detallesAlojamiento: any;
+  alojamiento: any;
   imagenesAlojamiento: any[] = [];
   baseUrl = 'http://127.0.0.1:8000';
+  imagenPrincipal: string = '';
+  numeroColumnas: number = 0;
 
   constructor(private route: ActivatedRoute, private alojamientoService: AlojamientoService) { 
     this.establecimientoId = 0;
-
   }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.establecimientoId = params['id'];
-      console.log(this.establecimientoId);
 
       // Llamar al servicio para obtener detalles según el ID del establecimiento
-      this.alojamientoService.obtenerAlojamiento(this.establecimientoId).subscribe(
-        (detalles) => {
-          this.detallesAlojamiento = detalles;
-          console.log(this.detallesAlojamiento)
-
-          this.alojamientoService.obtenerImagenesAlojamiento(this.establecimientoId).subscribe(
-              (imagenes) => {
-                this.imagenesAlojamiento = imagenes;
-                console.log(this.imagenesAlojamiento)
-                
-              },
-              (error) => {
-                console.error('Error al obtener imágenes del alojamiento', error);
-              }
-            );
-          },
-        (error) => {
-          console.error('Error al obtener detalles del alojamiento', error);
-        }
-      );
+      this.cargarDatos()
+      this.cargarImagenes();
     });
+
+    
+  }
+
+  cargarDatos() {
+    this.alojamientoService.obtenerAlojamiento(this.establecimientoId).subscribe(
+      (detalles) => {
+        this.alojamiento = detalles;
+        console.log(this.alojamiento);
+        },
+      (error) => {
+        console.error('Error al obtener detalles del alojamiento', error);
+      }
+    );
+  }
+
+  cargarImagenes() {
+    this.alojamientoService.obtenerImagenesAlojamiento(this.establecimientoId).subscribe(
+      (data) => {
+        this.imagenesAlojamiento = data;
+        // Establecer la imagen principal inicialmente como la primera imagen
+        this.imagenPrincipal = this.imagenesAlojamiento.length > 0 ? this.imagenesAlojamiento[0].imagen : '';
+        this.numeroColumnas = Math.ceil(this.imagenesAlojamiento.length / 2); // Calcula la mitad de la cantidad de imágenes
+        
+      },
+      (error) => {
+        console.error('Error al obtener imágenes:', error);
+      }
+    );
   }
   
+  seleccionarImagenPrincipal(imagen: any) {
+    // Cambiar la imagen principal al hacer clic en una miniatura
+    this.imagenPrincipal = imagen.imagen;
+    console.log('URL de la imagen principal:', this.baseUrl + this.imagenPrincipal);
+  }
 }
